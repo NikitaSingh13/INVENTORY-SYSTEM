4b1e10ff7ad4feb70c005ce62a20b5f7768a72b9
Nikita kumari <nikitasinghak257@gmail.com>
2026-01-27 17:18:32 +0530

    Prepare backend for deployment

--- server/server.js
+++ server/server.js
@@ -5,12 +5,14 @@ const productRoutes = require("./routes/products");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*", 
+}));
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
